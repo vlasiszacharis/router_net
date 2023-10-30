26a8c717f79496b49eea099512da59e2b50a3be2
vlasiszacharis <vlasisz983@gmail.com>
2023-10-30 15:24:41 +0200

    Router

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Link } from 'react-router-dom'
+import './Home.css'
+
+function Home() {
+  return (
+    <>
+    <div className='home'>
+         <Link to='/'>Home</Link>
+         
+    </div>
+    </>
+  )
+}
+
+export default Home
+
+
+
+
+
+
+
+
+
